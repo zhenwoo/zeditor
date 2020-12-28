ffb4f030882dae74fe2f0fc6d222b2d42b593c93
zhouqiansheng <zhouqs@zhulong.com.cn>
2020-12-28 18:27:47 +0800

    'init'

--- src/components/ScrollView/index.ts
+++ src/components/ScrollView/index.ts
@@ -4,24 +4,33 @@ import { getElementBySelector, getElementOffset } from '../../common/utills'
 export default class ScrollView {
     private ele: HTMLElement
     private readonly scrollView: HTMLElement
+    private readonly scrollViewAttr: DOMRect
+    private readonly scrollContent: HTMLElement
     constructor (props: ScrollViewConfig) {
         const { ele } = props
         this.ele = ele
         this.ele.innerHTML = view(props)
         this.scrollView = getElementBySelector('.zb-scroll-view') as HTMLElement
+        this.scrollContent = getElementBySelector('.zv-scroll-view-content') as HTMLElement
+        this.scrollViewAttr = getElementOffset(this.scrollView)
+        this.initData()
         this.initEvent()
     }
 
+    initData () {
+        const { height, width } = getElementOffset(this.scrollContent) as DOMRect
+        console.log(height, width)
+    }
+
     initEvent () {
-        console.log(this.scrollView)
         if (this.scrollView) {
             this.scrollView.addEventListener('wheel', this.handleWheel.bind(this), false)
         }
     }
 
     handleWheel (evt: MouseEvent) {
-        const eleOffset:DOMRect = getElementOffset(this.scrollView)
-        console.log(eleOffset)
+        console.log('++++++++++++++')
+        console.log(this.scrollViewAttr)
         console.log(evt)
     }
 }
--- src/components/ScrollView/view.ts
+++ src/components/ScrollView/view.ts
@@ -5,7 +5,7 @@ export const view = (prop: ScrollViewConfig):string => {
     const name = '周看盛'
     const template = `
         <div class="zb-scroll-view">
-            <div class="zv-scroll-view-content">{{name}}</div>
+            <div class="zv-scroll-view-content" style="height: 500px">{{name}}</div>
             <div class="zv-scroll-view-scroller-h">
                 <div class="zb-scroll-view-bar"></div>
             </div>
